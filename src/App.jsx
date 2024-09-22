de31ef68bda51f28f6c119f2094aa1138ac36533
ashish-kus <ashish.kus2408@gmail.com>
2024-09-22 12:26:14 +0530

    version 0.0.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AboutUs from "./components/AboutUs"
+import Headder from "./components/Headder"
+function App() {
+
+  return (
+    <>
+      <Headder />
+      <AboutUs />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,22 @@
+function AboutUs() {
+    return (
+        <>
+        <h1 className="text=white bg-pink-300">nandu</h1>
+            {/* <section className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-900 p-4">
+                <div className="text-center">
+                    <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-black dark:text-white">
+                        Now with <br className="block sm:hidden" /> Dark Mode.
+                    </h1>
+                    <p className="text-sm sm:text-base lg:text-lg text-gray-700 dark:text-gray-300 mt-4 max-w-md sm:max-w-lg lg:max-w-xl">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nobis laborum quo eum inventore nostrum aspernatur, fuga quis quod commodi esse nihil rem, repellat blanditiis temporibus quidem libero numquam ut.
+                    </p>
+                    <p className="text-sm sm:text-base lg:text-lg text-gray-700 dark:text-gray-300 mt-4 max-w-md sm:max-w-lg lg:max-w-xl">
+                        Odio praesentium ea accusamus libero harum eum reiciendis nobis veniam quia voluptatum assumenda voluptatibus perferendis, sunt iste laudantium, asperiores est itaque ex laboriosam consequatur deserunt natus sint qui nisi. Magni?
+                    </p>
+                </div>
+            </section> */}
+        </>
+    )
+}
+
+export default AboutUs;
